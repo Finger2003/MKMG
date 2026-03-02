39c80d77e2ee0799200014344dfb22f86dd937fb
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2026-03-02 18:18:24 +0100

    Add Elipsoid project

--- Elipsoid/framework.h
+++ Elipsoid/framework.h
@@ -1,11 +1,4 @@
-﻿#pragma once
+#pragma once
 
-#include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // Wyklucz rzadko używane rzeczy z nagłówków systemu Windows
-// Pliki nagłówkowe systemu Windows
-#include <windows.h>
-// Pliki nagłówkowe środowiska uruchomieniowego języka C
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
-#include <tchar.h>
+#include <string>
+#include <Windows.h>
